636bc0af14e49f2ad1de4fdf06893c4e77333279
Yasmin Azizah <95863438+yasmintuhfah@users.noreply.github.com>
2022-03-20 16:29:08 +0700

    Add files via upload

--- W6/W5_Aggregation/Komisi.java
+++ W6/W5_Aggregation/Komisi.java
@@ -1,1 +1,14 @@
+package W5_Aggregation;
+
+public class Komisi {
+	private String namaKomisi;
+	
+	public String getNamaKomisi() {
+		return namaKomisi;
+	}
+	
+	Komisi(String namaKomisi){
+		this.namaKomisi = namaKomisi;
+	}
+}
 
---
+++ W6/W5_Aggregation/MPK.java
@@ -0,0 +1,14 @@
+package W5_Aggregation;
+
+public class MPK {
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	MPK(String name){
+		this.name = name;
+	}
+}
+
