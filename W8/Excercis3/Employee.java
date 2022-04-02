891fd33688b41b8d03237012695c6517dc3da1b2
Yasmin Azizah <95863438+yasmintuhfah@users.noreply.github.com>
2022-04-03 04:09:01 +0700

    Add files via upload

---
+++ W8/Excercis3/Employee.java
@@ -0,0 +1,26 @@
+package W8_Inheritance3;
+
+public class Employee {
+	private String name;
+	private double salary;
+	private int hireday;
+	private int hiremonth;
+	private int hireyear;
+	
+	public Employee(String n, double s, int day, int month, int year) {
+		name = n; salary = s; hireday = day; hiremonth = month; hireyear = year;
+	}
+	
+	public void print() {
+		System.out.println(name + " " + salary + " " + hireYear());
+	}
+	
+	public void raiseSalary(double byPercent) {
+		salary *= 1 + byPercent / 100;
+	}
+	
+	public int hireYear() {
+		return hireyear;
+	}
+}
+
