2a11ff6810a5f22c42187127853a554907f7c9dc
Yasmin Azizah <95863438+yasmintuhfah@users.noreply.github.com>
2022-04-07 13:45:34 +0700

    Add files via upload

--- W9/7.3/Comission.java
+++ W9/7.3/Comission.java
@@ -1,1 +1,27 @@
+package W9;
 
+public class Comission extends Hourly {
+	private double total_sales;
+	private double commission_rate;
+	
+	public Comission(String eName, String eAddress, String ePhone, String socSecNumber, double rate, double commission) {
+		super(eName, eAddress, ePhone, socSecNumber, rate);
+		commission_rate = commission;
+	}
+	
+	public void addSales(double totalSales) {
+		total_sales = totalSales;
+	}
+	
+	public double pay() {
+		double payment = super.pay() + (total_sales * (1.0 + commission_rate));
+		return payment;
+	}
+	
+	public String toString() {
+		String result = super.toString();
+		result += "\nTotal Sale: " + total_sales;
+		return result;
+	}
+
+}
---
+++ W9/7.3/Firm.java
@@ -0,0 +1,8 @@
+package W9;
+
+public class Firm {
+	public static void main(String[] args) {
+		Staff personnel = new Staff();
+		personnel.payday();
+	}
+}
